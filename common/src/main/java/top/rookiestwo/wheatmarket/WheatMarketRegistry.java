020553db70d1e72f68817f9b7b3d66a9148e62d7
肉鸡二号 <rookies_two@yeah.net>
2025-03-31 00:20:20 +0800

    笔记本电脑方块和对应物品的注册修复

--- common/src/main/java/top/rookiestwo/wheatmarket/WheatMarketRegistry.java
+++ common/src/main/java/top/rookiestwo/wheatmarket/WheatMarketRegistry.java
@@ -3,17 +3,21 @@ package top.rookiestwo.wheatmarket;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 
 public class WheatMarketRegistry {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Wheatmarket.MOD_ID, Registries.ITEM);
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Wheatmarket.MOD_ID, Registries.BLOCK);
     public WheatMarketRegistry(){
         System.out.println("WheatMarket Registering...");
-        RegistrySupplier<Item> LAPTOP = ITEMS.register("laptop", () -> new Item(new Item.Properties()));
-        RegistrySupplier<Block> LAPTOP_BLOCK = BLOCKS.register("laptop_block", () -> new Block(Block.Properties.of()));
-        ITEMS.register();
+        RegistrySupplier<Block> LAPTOP_BLOCK = BLOCKS.register("laptop", () -> new Block(BlockBehaviour.Properties.of()
+                .sound(SoundType.METAL)
+        ));
+        RegistrySupplier<BlockItem> LAPTOP_ITEM = ITEMS.register("laptop", () -> new BlockItem(LAPTOP_BLOCK.get(), new Item.Properties()));
         BLOCKS.register();
+        ITEMS.register();
     }
 }

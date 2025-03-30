28425f440e1468e35c561eee9f0ae6f45da86ff9
肉鸡二号 <rookies_two@yeah.net>
2025-03-30 23:38:28 +0800

    注册了笔记本电脑方块

--- common/src/main/java/top/rookiestwo/wheatmarket/WheatMarketRegistry.java
+++ common/src/main/java/top/rookiestwo/wheatmarket/WheatMarketRegistry.java
@@ -4,13 +4,15 @@ import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 
 public class WheatMarketRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Wheatmarket.MOD_ID, Registries.ITEM);
-
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Wheatmarket.MOD_ID, Registries.BLOCK);
     public WheatMarketRegistry(){
         System.out.println("WheatMarket Registering...");
         RegistrySupplier<Item> LAPTOP = ITEMS.register("laptop", () -> new Item(new Item.Properties()));
+        RegistrySupplier<Block> LAPTOP_BLOCK = BLOCKS.register("laptop_block", () -> new Block(Block.Properties.of()));
         ITEMS.register();
     }
 }

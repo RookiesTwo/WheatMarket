09bda93ec16b6229cff7628950cd8400505b7c5e
肉鸡二号 <rookies_two@yeah.net>
2025-04-05 01:10:34 +0800

    Update CommandInterface.java

--- common/src/main/java/top/rookiestwo/wheatmarket/command/CommandInterface.java
+++ common/src/main/java/top/rookiestwo/wheatmarket/command/CommandInterface.java
@@ -4,6 +4,6 @@ import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSourceStack;
 
 public interface CommandInterface {
-    public void register();
-    public int run(CommandContext<CommandSourceStack> commandContext);
+    void register();
+    int run(CommandContext<CommandSourceStack> commandContext);
 }

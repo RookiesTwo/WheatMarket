73a285d03d30335dfd2261b304d8987520943299
肉鸡二号 <rookies_two@yeah.net>
2025-08-13 21:46:12 +0800

    初步添加搜索框，并添加商品的粗略渲染

--- common/src/main/java/top/rookiestwo/wheatmarket/client/gui/containers/MainContentContainer.java
+++ common/src/main/java/top/rookiestwo/wheatmarket/client/gui/containers/MainContentContainer.java
@@ -1,11 +1,13 @@
 package top.rookiestwo.wheatmarket.client.gui.containers;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
 import top.rookiestwo.wheatmarket.WheatMarket;
 import top.rookiestwo.wheatmarket.client.gui.WheatMarketMenuScreen;
 import top.rookiestwo.wheatmarket.client.gui.widgets.BlockBackgroundWidget;
@@ -14,7 +16,6 @@ import top.rookiestwo.wheatmarket.client.gui.widgets.buttons.ItemType;
 import top.rookiestwo.wheatmarket.client.gui.widgets.buttons.SortType;
 import top.rookiestwo.wheatmarket.client.gui.widgets.buttons.TradeType;
 
-@Environment(EnvType.CLIENT)
 public class MainContentContainer extends AbstractWidgetContainer{
 
     /**
@@ -24,7 +25,14 @@ public class MainContentContainer extends AbstractWidgetContainer{
     private static final ResourceLocation BOARD = ResourceLocation.fromNamespaceAndPath(WheatMarket.MOD_ID,"screen/main_menu/board");
     private static final ResourceLocation BUTTON = ResourceLocation.fromNamespaceAndPath(WheatMarket.MOD_ID,"screen/main_menu/button");
     private static final ResourceLocation WIDGET_BG=ResourceLocation.fromNamespaceAndPath(WheatMarket.MOD_ID,"screen/main_menu/widget_bg");
+    private static final ResourceLocation PAPER =ResourceLocation.fromNamespaceAndPath(WheatMarket.MOD_ID,"screen/main_menu/paper");
+    private static final ResourceLocation PAPER_WITH_WRINKLES =ResourceLocation.fromNamespaceAndPath(WheatMarket.MOD_ID,"screen/main_menu/paper_with_wrinkles");
+
+    //搜索框及其背景
     private BlockBackgroundWidget searchBackgroundWidget;
+    private BlockBackgroundWidget searchBarBackgroundWidget;
+    private EditBox searchEditBox;
+
     private BlockBackgroundWidget goodsBackgroundWidget;
 
     private FilterButton<TradeType> filter1Button;
@@ -33,6 +41,8 @@ public class MainContentContainer extends AbstractWidgetContainer{
 
     private static int containerPaddingX = 15;
     private static int containerPaddingY = 15;
+
+    private static int searchBarPaddingX = 8;
 
     private static int widgetCommonPadding = 5;
 
@@ -53,35 +63,18 @@ public class MainContentContainer extends AbstractWidgetContainer{
         searchBackgroundWidget = new BlockBackgroundWidget(x, y, width, 16, Component.empty(), 0.0f, 0.0f, BOARD);
         goodsBackgroundWidget = new BlockBackgroundWidget(x, y, width, 16, Component.empty(), 0.0f, 0.0f, BOARD);
 
-        filter1Button= new FilterButton<>(
-                x,
-                y,
-                defaultFilterWidth,
-                16,
-                Component.empty(),
-                BUTTON,
-                TradeType.ALL
-        );
-        filter2Button = new FilterButton<>(
-                x,
-                y,
-                defaultFilterWidth,
-                16,
-                Component.empty()
-                ,BUTTON
-                ,SortType.LAST_TRADE
-        );
-        filter3Button = new FilterButton<>(
-                x,
-                y,
-                defaultFilterWidth,
-                16,
-                Component.empty(),
-                BUTTON,
-                ItemType.ALL
-        );
+        filter1Button= new FilterButton<>(x, y, defaultFilterWidth, 16, Component.empty(), BUTTON, TradeType.ALL);
+        filter2Button = new FilterButton<>(x, y, defaultFilterWidth, 16, Component.empty(),BUTTON,SortType.LAST_TRADE);
+        filter3Button = new FilterButton<>(x, y, defaultFilterWidth, 16, Component.empty(), BUTTON, ItemType.ALL);
+
+        searchBarBackgroundWidget=new BlockBackgroundWidget(x, y, width, 16, Component.empty(), 0.0f, 0.0f, PAPER);
+        searchEditBox=new EditBox(Minecraft.getInstance().font, x, y, 0, 0, Component.translatable("gui.wheatmarket.searchbar"));
+        searchEditBox.setVisible(true);
+        searchEditBox.setBordered(false);
+        searchEditBox.setMaxLength(50);
+        searchEditBox.setTextColor(0x2B2D30);
         this.fitWidgets();
-        this.addButtons();
+        this.addWidgets();
     }
 
     @Override
@@ -107,6 +100,10 @@ public class MainContentContainer extends AbstractWidgetContainer{
 
         searchBackgroundWidget.setWidth(this.getWidth()-3*widgetCommonPadding-filter1Button.getWidth());
         searchBackgroundWidget.setHeight(Integer.max(defaultSearchBarHeight,this.getHeight()/10));
+        searchEditBox.setWidth(searchBackgroundWidget.getWidth()/5*4-searchBarPaddingX*2);
+        searchEditBox.setHeight(searchBackgroundWidget.getHeight()/5*4);
+        searchBarBackgroundWidget.setWidth(searchBackgroundWidget.getWidth()/5*4);
+        searchBarBackgroundWidget.setHeight(searchBackgroundWidget.getHeight()/5*4);
 
         goodsBackgroundWidget.setWidth(searchBackgroundWidget.getWidth());
         goodsBackgroundWidget.setHeight(this.getHeight()- searchBackgroundWidget.getHeight() - 3 * widgetCommonPadding);
@@ -123,16 +120,21 @@ public class MainContentContainer extends AbstractWidgetContainer{
 
         searchBackgroundWidget.setX(filter1Button.getX() + filter1Button.getWidth() + widgetCommonPadding);
         searchBackgroundWidget.setY(filter1Button.getY());
+        searchBarBackgroundWidget.setX(searchBackgroundWidget.getX()+searchBarPaddingX);
+        searchBarBackgroundWidget.setY(searchBackgroundWidget.getY()+searchBackgroundWidget.getHeight()/10);
+        searchEditBox.setX(searchBarBackgroundWidget.getX()+searchBarPaddingX);
+        searchEditBox.setY(searchBarBackgroundWidget.getY()+(searchBarBackgroundWidget.getHeight()-8)/2);
 
         goodsBackgroundWidget.setX(searchBackgroundWidget.getX());
         goodsBackgroundWidget.setY(searchBackgroundWidget.getY() + searchBackgroundWidget.getHeight() + widgetCommonPadding);
     }
 
-    private void addButtons(){
+    private void addWidgets(){
         if(this.screen instanceof WheatMarketMenuScreen){
             ((WheatMarketMenuScreen) this.screen).addButton(filter1Button);
             ((WheatMarketMenuScreen) this.screen).addButton(filter2Button);
-            ((WheatMarketMenuScreen) this.screen).addButton(filter3Button);;
+            ((WheatMarketMenuScreen) this.screen).addButton(filter3Button);
+            ((WheatMarketMenuScreen) this.screen).addWidget(searchEditBox);
         }
     }
 
@@ -148,5 +150,21 @@ public class MainContentContainer extends AbstractWidgetContainer{
         filter1Button.render(guiGraphics, mouseX, mouseY, partialTick);
         filter2Button.render(guiGraphics, mouseX, mouseY, partialTick);
         filter3Button.render(guiGraphics, mouseX, mouseY, partialTick);
+        //渲染搜索框
+        searchBarBackgroundWidget.render(guiGraphics, mouseX, mouseY, partialTick);
+        searchEditBox.render(guiGraphics, mouseX, mouseY, partialTick);
+        int x=goodsBackgroundWidget.getX()+8;
+        int y=goodsBackgroundWidget.getY() + 8;
+        int width=(goodsBackgroundWidget.getHeight()-24)/2;
+        for(int i=0;i<10;i++){
+            if(i==5)y= y+8+width;
+            if(i<5){
+                x =goodsBackgroundWidget.getX() + 8 + i * (width+8);
+            }
+            else{
+                x =goodsBackgroundWidget.getX() + 8 + (i-5) * (width+8);
+            }
+            guiGraphics.blitSprite(PAPER_WITH_WRINKLES, x, y, width, width);
+        }
     }
 }

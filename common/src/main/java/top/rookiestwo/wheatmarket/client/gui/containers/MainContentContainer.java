ad235f846d58ffe3e8659cb80b2c0c4821098bcd
肉鸡二号 <rookies_two@yeah.net>
2025-08-14 15:29:25 +0800

    为商品悬挂贴图添加随机性

--- common/src/main/java/top/rookiestwo/wheatmarket/client/gui/containers/MainContentContainer.java
+++ common/src/main/java/top/rookiestwo/wheatmarket/client/gui/containers/MainContentContainer.java
@@ -4,11 +4,8 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.FormattedCharSequence;
 import top.rookiestwo.wheatmarket.WheatMarket;
 import top.rookiestwo.wheatmarket.client.gui.WheatMarketMenuScreen;
 import top.rookiestwo.wheatmarket.client.gui.widgets.BlockBackgroundWidget;
@@ -28,13 +25,12 @@ public class MainContentContainer extends AbstractWidgetContainer{
     private static final ResourceLocation BUTTON = ResourceLocation.fromNamespaceAndPath(WheatMarket.MOD_ID,"screen/main_menu/button");
     private static final ResourceLocation WIDGET_BG=ResourceLocation.fromNamespaceAndPath(WheatMarket.MOD_ID,"screen/main_menu/widget_bg");
     private static final ResourceLocation PAPER =ResourceLocation.fromNamespaceAndPath(WheatMarket.MOD_ID,"screen/main_menu/paper");
-    private static final ResourceLocation PAPER_WITH_WRINKLES =ResourceLocation.fromNamespaceAndPath(WheatMarket.MOD_ID,"textures/gui/paper_with_wrinkles.png");
+    private static final ResourceLocation[] PAPER_WITH_WRINKLES =new ResourceLocation[7];
 
     //搜索框及其背景
     private BlockBackgroundWidget searchBackgroundWidget;
     private BlockBackgroundWidget searchBarBackgroundWidget;
-    private EditBox searchEditBox;
-    private WheatEditBox searchEditBox2;
+    private WheatEditBox searchEditBox;
 
     private BlockBackgroundWidget goodsBackgroundWidget;
 
@@ -58,6 +54,10 @@ public class MainContentContainer extends AbstractWidgetContainer{
     public MainContentContainer(int x, int y, int width, int height, Component message, Screen screen,AbstractWidgetContainer parentContainer) {
         super(x, y, width, height, message, screen);
 
+        for(int i=0;i<7;i++){
+            PAPER_WITH_WRINKLES[i]=ResourceLocation.fromNamespaceAndPath(WheatMarket.MOD_ID,"textures/gui/paper_with_wrinkles_"+i+".png");
+        }
+
         this.parentContainer = parentContainer;
 
         this.setX(containerPaddingX);
@@ -71,12 +71,7 @@ public class MainContentContainer extends AbstractWidgetContainer{
         filter3Button = new FilterButton<>(x, y, defaultFilterWidth, 16, Component.empty(), BUTTON, ItemType.ALL);
 
         searchBarBackgroundWidget=new BlockBackgroundWidget(x, y, width, 16, Component.empty(), 0.0f, 0.0f, PAPER);
-        //searchEditBox=new WheatEditBox(Minecraft.getInstance().font, x, y, 0, 0, Component.translatable("gui.wheatmarket.searchbar"));
-        //searchEditBox.setVisible(true);
-        //searchEditBox.setBordered(false);
-        //searchEditBox.setMaxLength(50);
-        //searchEditBox.setTextColor(0x2B2D30);
-        searchEditBox2=new WheatEditBox(Minecraft.getInstance().font, x, y, 0, 0, Component.translatable("gui.wheatmarket.searchbar"));
+        searchEditBox =new WheatEditBox(Minecraft.getInstance().font, x, y, 0, 0, Component.translatable("gui.wheatmarket.searchbar"));
         this.fitWidgets();
         this.addWidgets();
     }
@@ -106,8 +101,8 @@ public class MainContentContainer extends AbstractWidgetContainer{
         searchBackgroundWidget.setHeight(Integer.max(defaultSearchBarHeight,this.getHeight()/10));
         //searchEditBox.setWidth(searchBackgroundWidget.getWidth()/5*4-searchBarPaddingX*2);
         //searchEditBox.setHeight(searchBackgroundWidget.getHeight()/5*4);
-        searchEditBox2.setWidth(searchBackgroundWidget.getWidth()/5*4-searchBarPaddingX*2);
-        searchEditBox2.setHeight(searchBackgroundWidget.getHeight()/5*4);
+        searchEditBox.setWidth(searchBackgroundWidget.getWidth()/5*4-searchBarPaddingX*2);
+        searchEditBox.setHeight(searchBackgroundWidget.getHeight()/5*4);
         searchBarBackgroundWidget.setWidth(searchBackgroundWidget.getWidth()/5*4);
         searchBarBackgroundWidget.setHeight(searchBackgroundWidget.getHeight()/5*4);
 
@@ -130,8 +125,8 @@ public class MainContentContainer extends AbstractWidgetContainer{
         searchBarBackgroundWidget.setY(searchBackgroundWidget.getY()+searchBackgroundWidget.getHeight()/10);
         //searchEditBox.setX(searchBarBackgroundWidget.getX()+searchBarPaddingX);
         //searchEditBox.setY(searchBarBackgroundWidget.getY()+(searchBarBackgroundWidget.getHeight()-8)/2);
-        searchEditBox2.setX(searchBarBackgroundWidget.getX()+searchBarPaddingX);
-        searchEditBox2.setY(searchBarBackgroundWidget.getY()+(searchBarBackgroundWidget.getHeight()-8)/2);
+        searchEditBox.setX(searchBarBackgroundWidget.getX()+searchBarPaddingX);
+        searchEditBox.setY(searchBarBackgroundWidget.getY()+(searchBarBackgroundWidget.getHeight()-8)/2);
 
         goodsBackgroundWidget.setX(searchBackgroundWidget.getX());
         goodsBackgroundWidget.setY(searchBackgroundWidget.getY() + searchBackgroundWidget.getHeight() + widgetCommonPadding);
@@ -142,8 +137,7 @@ public class MainContentContainer extends AbstractWidgetContainer{
             ((WheatMarketMenuScreen) this.screen).addButton(filter1Button);
             ((WheatMarketMenuScreen) this.screen).addButton(filter2Button);
             ((WheatMarketMenuScreen) this.screen).addButton(filter3Button);
-            //((WheatMarketMenuScreen) this.screen).addWidget(searchEditBox);
-            ((WheatMarketMenuScreen) this.screen).addWidget(searchEditBox2);
+            ((WheatMarketMenuScreen) this.screen).addWidget(searchEditBox);
         }
     }
 
@@ -162,7 +156,7 @@ public class MainContentContainer extends AbstractWidgetContainer{
         //渲染搜索框
         searchBarBackgroundWidget.render(guiGraphics, mouseX, mouseY, partialTick);
         //searchEditBox.render(guiGraphics, mouseX, mouseY, partialTick);
-        searchEditBox2.render(guiGraphics, mouseX, mouseY, partialTick);
+        searchEditBox.render(guiGraphics, mouseX, mouseY, partialTick);
         int x=goodsBackgroundWidget.getX()+8;
         int y=goodsBackgroundWidget.getY() + 8;
         int width=64;
@@ -174,7 +168,7 @@ public class MainContentContainer extends AbstractWidgetContainer{
             else{
                 x =goodsBackgroundWidget.getX() + 8 + (i-5) * (width+8);
             }
-            guiGraphics.blit(PAPER_WITH_WRINKLES,x,y,0,0,width,width,64,64);
+            guiGraphics.blit(PAPER_WITH_WRINKLES[i%7],x,y,0,0,width,width,64,64);
         }
     }
 }
